175f963506ee9a0b9c9033cdfe5a4db5b8e4ab2b
Jomai <jomai.jomai1243@gmail.com>
2026-05-09 21:16:35 +0300

    fix render python loop

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 print("BOT STARTING...")
 
 import os
-TOKEN = os.getenv("8740241159:AAEUJh_5NK2OrHbWhSvnPmIHk1Wcv9d7-eo")
+TOKEN = os.getenv("8740241159:AAEvzuZt4mF7JSWLtKidr6Hy_o93Kd2uFxg")
 
 teachers = set()
 students = set()
